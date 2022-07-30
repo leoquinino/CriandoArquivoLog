8b9683e5fdc1097c19dc145692a0743321b65ddf
leonardo <leonardo.quinino@gmail.com>
2022-07-29 22:43:56 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppConfigurationManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string PathFile = ConfigurationManager.AppSettings["PathFileLog"];
+            
+            richTextBox1.Text = "";            
+            richTextBox1.AppendText("Nome do Usuário: "                   + ConfigurationManager.AppSettings["Login"]);
+            richTextBox1.AppendText(Environment.NewLine + "Senha :"       + ConfigurationManager.AppSettings["Senha"]);
+            richTextBox1.AppendText(Environment.NewLine + "Email :"       + ConfigurationManager.AppSettings["Email"]);            
+            richTextBox1.AppendText(Environment.NewLine + "LogAtivo :"    + ConfigurationManager.AppSettings["LogAtivo"]);
+            richTextBox1.AppendText(Environment.NewLine + "PathFileLog :" + PathFile);
+
+            System.Text.StringBuilder Mensagem = new System.Text.StringBuilder();
+
+            for (var iConta = 0; iConta <= richTextBox1.Lines.Count() - 1; iConta++)
+            {
+                Mensagem.Append(richTextBox1.Lines[iConta].ToString() + Environment.NewLine);
+            }
+
+            //MessageBox.Show(Mensagem.ToString(), " TEXTO ", MessageBoxButtons.OK);
+
+            GravaLog MeuLog = new GravaLog();
+
+            MeuLog.grava(Mensagem.ToString(), PathFile);
+
+
+            /*
+            var MyConfig = ConfigurationManager.GetSection("appLogErros/Configuracoes") as NameValueCollection;                        
+
+            if (MyConfig == "") 
+            {
+                Console.WriteLine("A seção <appLogErros> não esta definida");
+            }
+            else
+            {                
+                 foreach (var key in MyConfig.AllKeys)
+                {
+                    Console.WriteLine(key + " = " + MyConfig[key]);
+                }                
+            }
+            */
+
+        }
+    }
+
+    internal class NameValueFileCollection
+    {
+        internal object Get(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ clas/ClassGravaLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+
+public class GravaLog
+{     
+    public void grava(string Mensagem, string path_log)
+    {
+        String data = DateTime.Now.ToString("yyyy-MM-dd");
+        String hora = DateTime.Now.ToString("hh-mm-ss");
+        String computador = Dns.GetHostName();
+        String nomeLog = path_log + data + hora + ".log";
+
+        if (!Directory.Exists(path_log))
+        {
+            Directory.CreateDirectory(path_log);
+        }
+
+        if (Directory.Exists(path_log))
+        {
+            using (StreamWriter outputFile = new StreamWriter(nomeLog, true))
+            {
+                outputFile.WriteLine(data + " " + hora + " (" + computador + ")" + Environment.NewLine + Mensagem);
+            }
+        }
+    }
+}
+
